9c14a37350b2e3f8780dd028e5edfdf8e1a13c90
joaks <joaquinsepulvedaraya@gmail.com>
2016-03-03 20:55:51 -0300

    pregunta 1 final

---
+++ pregunta1.rb
@@ -0,0 +1,7 @@
+a = [1,2,3]
+b = [:azul, :rojo, :amarillo]
+c = ["Tacos", "Quesadillas", "Hamburguesas"]
+
+puts "#{a[0]} #{b[0]} #{c[0]}" 
+puts "#{a[1]} #{b[1]} #{c[1]}" 
+puts "#{a[2]} #{b[2]} #{c[2]}" 
