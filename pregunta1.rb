1175aafcb6ada0ac604fb09f75b96a8e83862c0f
joaks <joaquinsepulvedaraya@gmail.com>
2016-03-03 21:12:07 -0300

    arreglando la 1

--- pregunta1.rb
+++ pregunta1.rb
@@ -1,7 +1,17 @@
+require "pp"
 a = [1,2,3]
 b = [:azul, :rojo, :amarillo]
 c = ["Tacos", "Quesadillas", "Hamburguesas"]
 
-puts "#{a[0]} #{b[0]} #{c[0]}" 
-puts "#{a[1]} #{b[1]} #{c[1]}" 
-puts "#{a[2]} #{b[2]} #{c[2]}" 
+array=[a,b,c]
+
+array.each do |x|
+
+	print "#{x[0]} "
+	print "#{x[1]} "
+	print "#{x[2]} "
+	
+end
+
+
+
