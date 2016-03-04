2b3c701dd5acb5a7a27a1b5b9baac4def95b188f
joaks <joaquinsepulvedaraya@gmail.com>
2016-03-03 21:47:45 -0300

    pregunta 4 lista

---
+++ pregunta4.rb
@@ -0,0 +1,19 @@
+#Utilizando como base el arreglo de la pregunta anterior, 
+#crear un arreglo nuevo remplazando todos los
+#números y símbolos por string
+require "pp"
+a = [1,2,3]
+b = [:azul, :rojo, :amarillo]
+c = ["Tacos", "Quesadillas", "Hamburguesas"]
+
+array=[a,b,c]
+arrayFinal =[]
+i=0
+while i < array.length
+	array.each do |x|
+		 arrayFinal.push(x[i].to_s)
+	end
+	
+	i +=1
+end
+print arrayFinal
