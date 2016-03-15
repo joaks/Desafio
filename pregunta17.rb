02c6e0c339ac411e78c583e4948b3f919d10141c
Francisca Medina <fbmedina@uc.cl>
2016-03-15 18:07:59 -0300

    se está re-elaborando la 17

--- pregunta17.rb
+++ pregunta17.rb
@@ -1,14 +1,27 @@
 require "pp"
-mappin = {Arica: ["Arica", "Camarones", "Putre", "General Lagos"], Tarapaca: ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"]}
+mappin = {Arica: ["Arica", "Camarones", "Putre", "General Lagos"],
+  Tarapaca: ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"],
+  Antofagasta: ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal"]}
 
-puts "A cuál región le quieres agregar una ciudad"
+
+puts "¿Qué quieres hacer?\na) Agregar una ciudad.\nb) Quitar una ciudad\nc) Agregar una región\nd) Quitar una región"
 user_choice = gets.chomp
 
-mappin.each do |k,v|
-  if user_choice.to_sym == k
-    puts "Escribe el nombre de la ciudad"
-    user_choice2 = gets.chomp
-    v.push(user_choice2)
+
+if user_choice == "a"
+
+  puts "A cuál región le quieres agregar una ciudad"
+  user_region = gets.chomp
+
+  mappin.each do |k,v|
+    if user_region.to_sym == k
+      puts "Escribe el nombre de la ciudad"
+      user_city = gets.chomp
+      v.push(user_city)
+
+    else
+      puts "No está registrada esa región en el mapa"
+    end
   end
 end
 
